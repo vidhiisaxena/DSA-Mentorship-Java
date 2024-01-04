2cc481e5422677527a8b529ea80229a20d7d83a4
Vidhi Saxena <144513634+vidhiisaxena@users.noreply.github.com>
2024-01-04 23:58:12 +0530

    Create a2_q13.java

---
+++ assign2/a2_q13.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class a2_q13
+{
+	public static void main(String[]args)
+	{
+		try(Scanner scn = new Scanner(System.in)){;
+		int n = scn.nextInt();
+		
+		for (int i=0;i<n;i++)
+		{
+			int k = 1;
+			for ( int j=0;j<=i;j++)
+			{
+				System.out.print(m+" ");
+				int k1 = k*(i-j)/(j+1);
+				k=k1;
+			}
+			System.out.println();
+		}
+	}
+}
+}
