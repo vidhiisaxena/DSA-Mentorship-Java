0df33325f33887e26b85edd1c81550ee604f3bdb
Vidhi Saxena <144513634+vidhiisaxena@users.noreply.github.com>
2024-01-04 23:36:09 +0530

    Create a2_q4.java

---
+++ a2_q4.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class a2_q4
+{
+	public static void main(String[]args) {
+		try(Scanner scn = new Scanner(System.in)){;
+		int n= scn.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=(i-1);j++)
+			{
+				System.out.print(" ");
+			}
+			for (int k=1;k<=(n+1-i);k++ )
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	 }
+  }
+}
