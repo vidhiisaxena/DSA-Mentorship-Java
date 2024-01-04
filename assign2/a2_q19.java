bbf7f09415702482304e8e36a6e8d4029246c737
Vidhi Saxena <144513634+vidhiisaxena@users.noreply.github.com>
2024-01-05 00:08:17 +0530

    Create a2_q19.java

---
+++ assign2/a2_q19.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+public class a2_q19
+{
+	public static void main(String[]args)
+{
+		try(Scanner scn= new Scanner(System.in)){;
+		int n = scn.nextInt();
+		for(int i=1;i<=n;i++)
+		{   for (int j=1;j<=n;j++) {
+			if(i==1)
+			{
+				if(j==n || j<=n/2+1)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			 else if(i<=n/2)
+			{
+				if(j==n || j==n/2+1)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			else if(i==n/2+1)
+			{
+				System.out.print("*");
+				
+			}
+			else if(i<n)
+			{
+				if (j==1 || j==n/2+1)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");}
+			}
+			else
+			{
+				if (j==1 || j>=n/2+1)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");}
+			}
+			
+		} System.out.println();
+		}
+  }
+}
+}
